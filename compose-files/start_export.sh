64cf1c805dcd69eb7e787e01e3cbdaa7a77348cc
David Chang <david@iotechsys.com>
2019-07-14 23:43:34 +0000

    Update docker-compose.yml

---
+++ compose-files/start_export.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+FLAGS="-d --no-deps"
+
+docker-compose up $FLAGS consul
+sleep 3
+
+docker-compose up $FLAGS config-seed
+sleep 3
+
+docker-compose up $FLAGS volume
+sleep 3
+
+docker-compose up $FLAGS redis
+sleep 7 
+
+docker-compose up $FLAGS logging
+sleep 3
+
+docker-compose up $FLAGS data
+sleep 3
+
+docker-compose up $FLAGS metadata
+sleep 3
+
+docker-compose up $FLAGS command
+sleep 3
+
+docker-compose up $FLAGS device-random
+sleep 3
+
+docker-compose up $FLAGS export-client
+sleep 3
+
+#docker-compose up $FLAGS export-distro
+#sleep 3
+
