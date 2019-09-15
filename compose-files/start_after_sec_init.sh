a8ae54600f7afc5923877ccd0fdf0299e5da2cd0
David Chang <david@iotechsys.com>
2019-09-15 07:05:53 +0000

    Add start_after_sec_init.sh

---
+++ compose-files/start_after_sec_init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+FLAGS="-d --no-deps"
+
+docker-compose up $FLAGS redis
+sleep 7 
+
+docker-compose up $FLAGS logging
+sleep 3
+
+docker-compose up $FLAGS data
+sleep 3
+
+docker-compose up $FLAGS metadata
+sleep 3
+
+docker-compose up $FLAGS command
+sleep 3
+
+#docker-compose up $FLAGS device-virtual
+#sleep 3
+
+docker-compose up $FLAGS device-random
